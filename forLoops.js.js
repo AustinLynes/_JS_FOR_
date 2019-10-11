895a53496264f8a68d03297582f09f6512ecd7be
AustinLynes <austinlynes94@gmail.com>
2019-10-11 13:03:18 -0700

    created EX

---
+++ forLoops.js.js
@@ -0,0 +1,117 @@
+/*
+
+*/
+
+// Example:
+// Write a for loop that logs the numbers 0 through 5
+function loopToFive() {
+    for (let i = 0; i <= 5; i++) {
+       console.log(i)
+    }
+}
+loopToFive()
+// 1.
+// Write a for loop that runs the same logic
+// as the following code
+// 
+// let i = 0;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+// console.log(i);
+// i++;
+
+
+// 2.
+// Write a for loop that runs the same logic
+// as the following code
+// 
+// let i = 10;
+// console.log(i+3);
+// i--;
+// console.log(i+3);
+// i--;
+// console.log(i+3);
+// i--;
+// console.log(i+3);
+// i--;
+
+
+
+// 3.
+// Write a for loop that logs the numbers 0 through 10
+
+
+
+
+// 4.
+// Write a loop that logs the numbers 10 through 0
+
+// 5.
+// Write a loop that logs the word "Hippopotamus" 17 times.  
+
+// 6.
+// Write a for loop that logs 2, 4, 6, 8, 10
+
+// 7.
+// Write a loop that logs the numbers 2, 4, 6, 8.
+// Do not edit the existing code.
+
+
+// 8.
+// Write a loop that logs the numbers 5 through 10. 
+// Do not edit the existing code.
+// Hint: Use an if/else statement
+
+
+// 9. 
+// Write a loop that logs "Marco!" when i is even,
+// "Polo!" when i is odd.
+// Do not edit the existing code.
+// Hint 1: Use an if/else statement
+// Hint 2: Google the mod operator (%)
+
+
+
+// STRETCH PROBLEMS (NOT REQUIRED)
+
+// 10.
+// Find a second way to write the loop from question 8. 
+// Do not edit the existing code.
+
+
+//#region question 11
+// 11. 
+// Write a loop that logs the numbers 0 through 10.
+// Finish the code in the parentheses 
+// Do not edit any other existing code.
+
+for (let i; i; i) {
+
+    //Do not add or edit code below here!
+    if (i < 0) {
+        continue;
+    } else if (i > 50) {
+        break;
+    } else {
+        console.log(i / 5);
+    }
+}
+//#endregion
+// 12.
+// Imagine you are throwing two six sided dice.
+// Write a lested loop that logs every possible combination you could roll.
+// Your log statements should look like:
+// 1 & 1
+// 1 & 2
+// 1 & 3
+// 1 & 4
+// 1 & 5
+// 1 & 6
+// 2 & 1
+// 2 & 2
+// 2 & 3
+// etc.
+
+
